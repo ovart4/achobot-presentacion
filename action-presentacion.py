d8ee5a46030e945c9e319f50bf0ecdc876fc4374
ovart4 <49517750+ovart4@users.noreply.github.com>
2019-09-25 23:27:59 +0200

    Update action-presentacion.py

--- action-presentacion.py
+++ action-presentacion.py
@@ -11,7 +11,7 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 def intent_received(hermes, intent_message):
 
     if intent_message.intent.intent_name == 'josenka:presentacion':
-        sentence = 'Hola, soy Lola. Soy una plataforma robótica de asistencia a personas de edad avanzada. He sido diseñado por un estudiante de la UPCT y dispongo de una interfaz de voz, una interfaz gráfica y un diseño muy bonito.'
+        sentence = 'Hola, soy Lola. Soy una plataforma robótica de asistencia a personas de edad avanzada. He sido diseñado por un estudiante de la Politécnica de Cartagena. Dispongo de una interfaz de voz, una interfaz gráfica y un diseño muy bonito.'
     else:
         return
     
